23d5443fd55de1f77fc87a01f64ebff14c075287
gfsp2001 <gfsp2001@hotmail.com>
2023-06-29 02:13:24 -0500

    lista asincrona realizada, Giancarlo S.

--- app/src/main/java/com/example/pokedex/viewModels/StartViewModel.java
+++ app/src/main/java/com/example/pokedex/viewModels/StartViewModel.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.google.common.util.concurrent.SettableFuture;
 
 import com.example.pokedex.entities.PokemonResponse;
 import com.example.pokedex.models.PokemonRepository;
@@ -15,6 +16,13 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -30,26 +38,52 @@ public class StartViewModel extends ViewModel {
     }
 
     public void setInitialPokemonResponse(String apiUrl) {
+        final SettableFuture<Void> future = SettableFuture.create();
         pokemonRepository.repoBasicResponse(apiUrl, new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.getStackTrace();
+                e.printStackTrace();
+                future.setException(e);
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                String responseBody = response.body().string();
-                JsonElement jsonElement = JsonParser.parseString(responseBody);
-                JsonArray jsonResult = jsonElement.getAsJsonObject().getAsJsonArray("results");
+                try {
+                    String responseBody = response.body().string();
+                    JsonElement jsonElement = JsonParser.parseString(responseBody);
+                    JsonArray jsonResult = jsonElement.getAsJsonObject().getAsJsonArray("results");
 
-                for (JsonElement result : jsonResult) {
-                    String pokemonName = result.getAsJsonObject().get("name").getAsString();
-                    String pokemonUrl = result.getAsJsonObject().get("url").getAsString();
-                    PokemonResponse pokemonResponse = new PokemonResponse(pokemonName, pokemonUrl, 0, null, null);
-                    setPartialPokemonResponse(pokemonResponse);
+                    List<Callable<Void>> tasks = new ArrayList<>();
+                    for (JsonElement result : jsonResult) {
+                        String pokemonName = result.getAsJsonObject().get("name").getAsString();
+                        String pokemonUrl = result.getAsJsonObject().get("url").getAsString();
+                        PokemonResponse pokemonResponse = new PokemonResponse(pokemonName, pokemonUrl, 0, null, null);
+                        tasks.add(() -> {
+                            setPartialPokemonResponse(pokemonResponse);
+                            return null;
+                        });
+                    }
+
+                    ExecutorService executor = Executors.newCachedThreadPool();
+                    List<Future<Void>> futures = executor.invokeAll(tasks);
+                    executor.shutdown();
+
+                    for (Future<Void> future : futures) {
+                        future.get(); // Esperar a que se complete cada tarea
+                    }
+
+                    future.set(null);
+                } catch (Exception e) {
+                    future.setException(e);
                 }
             }
         });
+
+        try {
+            future.get(); // Esperar a que se complete la respuesta
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void setPartialPokemonResponse(PokemonResponse pokemonResponse) {
